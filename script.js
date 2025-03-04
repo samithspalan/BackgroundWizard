6f5cb9aa0cfc4aa413c0583d05084088dc6e00fd
samithspalan <samithpoojary12345@gmail.com>
2025-03-04 23:51:06 +0530

    Add index.html and script.js files

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log("hello");
+let fileinput=document.getElementById("filepicker");
+let fimage=document.getElementById("fimage");
+let transimg=document.getElementById("transimg");
+let inputimage=document.getElementById("input-image");
+let innerimage=document.querySelector(".inner-image");
+let btn=document.getElementById("btn");
+let image=null;
+innerimage.addEventListener("click",()=>{
+    fileinput.click();
+})
+fileinput.addEventListener("change",()=>{
+ image=fileinput.files[0];
+ if(!fileinput){
+    return;
+ }
+ let reader=new FileReader();
+ reader.onload=(e)=>{
+    inputimage.src=`data:${fileinput.type};base64,${e.target.result.split(",")[1]}`;
+    inputimage.style.display="block";
+ }
+ reader.readAsDataURL(image);
+});
+
