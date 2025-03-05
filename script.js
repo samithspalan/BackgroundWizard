627e6b2ebbc95295a8ad279436bf5a4aa518ba17
samithspalan <samithpoojary12345@gmail.com>
2025-03-06 00:32:19 +0530

    initial commit

--- script.js
+++ script.js
@@ -6,16 +6,28 @@ let innerimage=document.querySelector(".inner-image");
 let btn=document.querySelector(".btn");
 let plus=document.getElementById("plus");
 let span=document.querySelector("span");
+let downloadbtn=document.getElementById("download");
+let resetbtn=document.getElementById("reset");
 let image=null;
-let url=null
-let genimg=document.querySelector("#generated-image");
+let url=null;
+let style2=document.querySelector(".style2");
+let result=document.querySelector(".result");
+let originalimage=document.querySelector(".resultimg1 img");
+let generatedimg=document.querySelector(".resultimg2 img");
 function handleupload(){
    if (!image) {
        alert("Please select an image first");
        return;
    }
 
-   
+
+   fetch("https://api.remove.bg/v1.0/removebg", {
+      method: "POST",
+      headers: { 
+         "X-Api-Key": api
+      },
+      body: formdata
+   })
    .then(function(response){
        if (!response.ok) {
            return response.json().then(error => Promise.reject(error));
@@ -23,8 +35,10 @@ function handleupload(){
        return response.blob();
    })
    .then(function(blob){
+      style2.style.display="none";
+      result.style.display="flex";
        url = URL.createObjectURL(blob);
-       genimg.src = url;
+       generatedimg.src = url;
    })
    .catch(function(error) {
        console.error('Error:', error);
@@ -45,9 +59,28 @@ fileinput.addEventListener("change",()=>{
     inputimage.style.display="block";
     plus.style.display="none";
     span.style.display="none";
+    originalimage.src=`data:${fileinput.type};base64,${e.target.result.split(",")[1]}`;
+
  }
  reader.readAsDataURL(image);
 });
 btn.addEventListener("click",()=>{
       handleupload();
 })
+function downloadimage(){
+    fetch(url)
+    .then(res=>res.blob())
+    .then(file=>{
+        let a =document.createElement("a");
+        a.href=URL.createObjectURL(file);
+        a.download="image.png";
+        a.click();
+    })
+   .catch();
+}
+downloadbtn.addEventListener("click",()=>{
+    downloadimage();
+});
+resetbtn.addEventListener("click",()=>{
+   window.location.reload();
+});
